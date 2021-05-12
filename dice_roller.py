d150da73fa07bbe06ef8150ef1b29e417eea0499
IlonaLouckova <Ilona.Louckova@gmail.com>
2021-05-12 22:17:09 +0200

    Add player name input

--- dice_roller.py
+++ dice_roller.py
@@ -1,4 +1,5 @@
 import random
+player_name = input('Who is rolling dice today?')
 dice_rolls = int(input('How many dice would you like to roll? '))
 dice_size = int(input('How many sides are the dice? '))
 dice_sum = 0
@@ -6,10 +7,10 @@ for i in range(0, dice_rolls):
     roll = random.randint(1, dice_size)
     dice_sum += roll
     if roll == 1:
-        print(f'You rolled a {roll}! Critical Fail')
+        print(f'{player_name} rolled a {roll}! Critical Fail')
     elif roll == dice_size:
-        print(f'You rolled a {roll}! Critical Success!')
+        print(f'{player_name} rolled a {roll}! Critical Success!')
     else:
-          print(f'You rolled a {roll}')
-if dice_rolls < 1:    
-    print(f'You have rolled a total of {dice_sum}')
+          print(f'{player_name} rolled a {roll}')
+if dice_rolls > 1:    
+    print(f'{player_name} has rolled a total of {dice_sum}')
