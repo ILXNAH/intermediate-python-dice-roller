6db9fe64e29ffc8e74cfca0b145b681fbd2741c7
IlonaLouckova <Ilona.Louckova@gmail.com>
2021-05-15 04:02:19 +0200

    Add rematch option

--- dice_roller.py
+++ dice_roller.py
@@ -16,34 +16,34 @@ if player2_var == True:
 dice_rolls = int(input('How many dice would you like to roll? '))
 dice_size = int(input('How many sides are the dice? '))
 dice_sum_p1 = 0
-def player1_roll():
+def player1_rolls():
     for i in range(0, dice_rolls):
         global player1_roll
         player1_roll = random.randint(1, dice_size)
         global dice_sum_p1
         dice_sum_p1 += player1_roll
         if player1_roll == 1:
-            print(f'{player1_name} rolled a {player1_roll}! Critical Fail')
+            print(f'{player1_name} rolled a {player1_roll}! Critical Fail!')
         elif player1_roll == dice_size:
             print(f'{player1_name} rolled a {player1_roll}! Critical Success!')
         else:
             print(f'{player1_name} rolled a {player1_roll}')
 dice_sum_p2 = 0
-def player2_roll():
+def player2_rolls():
     for i in range(0, dice_rolls):
         global player2_roll
         player2_roll = random.randint(1, dice_size)
         global dice_sum_p2 
         dice_sum_p2 += player2_roll
         if player2_roll == 1:
-            print(f'{player2_name} rolled a {player2_roll}! Critical Fail')
+            print(f'{player2_name} rolled a {player2_roll}! Critical Fail!')
         elif player2_roll == dice_size:
             print(f'{player2_name} rolled a {player2_roll}! Critical Success!')
         else:
             print(f'{player2_name} rolled a {player2_roll}')
-player1_roll()
+player1_rolls()
 if player2_var == True:
-    player2_roll()
+    player2_rolls()
 #to define results for player1 and player2
 if dice_rolls > 1:
     player1_result = dice_sum_p1
@@ -51,15 +51,21 @@ if dice_rolls > 1:
 else:
     player1_result = player1_roll
     player2_result = player2_roll
-#to define the comparison function
+#to define comparison function and rematch option
 def compare_results():
     if player1_result > player2_result:
-        print (f"The WINNER is {player1_name} with the score of {dice_sum_p1}, congrats! \U0001F973")
+        print (f'The WINNER is {player1_name} with the score of {dice_sum_p1}, congrats! \U0001F973')
     elif player2_result > player1_result:
-        print (f"The WINNER is {player2_name} with the score of {dice_sum_p2}, congrats! \U0001F973")
+        print (f'The WINNER is {player2_name} with the score of {dice_sum_p2}, congrats! \U0001F973')
     else:
-        print (f"It's a TIE! Ouch!")
-#to compare the scores 
+        print ("Well, that's a bummer, it's a TIE. \U0001F631")
+    global rematch
+    rematch = yesorno("Do you want a rematch")
+    if rematch == True:
+        player1_rolls()
+        player2_rolls()
+        compare_results()
+#final scores and comparison
 if player2_var == True:
     compare_results()
 elif player2_var == False and dice_rolls > 1:
