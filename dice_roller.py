0de08e75540490476ed73cac2380dd2007eda8be
IlonaLouckova <Ilona.Louckova@gmail.com>
2021-05-15 03:21:00 +0200

    Add score comparison

--- dice_roller.py
+++ dice_roller.py
@@ -15,32 +15,52 @@ if player2_var == True:
     player2_name = input('Who?')
 dice_rolls = int(input('How many dice would you like to roll? '))
 dice_size = int(input('How many sides are the dice? '))
-def rolling1():
-    dice_sum = 0
+dice_sum_p1 = 0
+def player1_roll():
     for i in range(0, dice_rolls):
+        global player1_roll
         player1_roll = random.randint(1, dice_size)
-        dice_sum += player1_roll
+        global dice_sum_p1
+        dice_sum_p1 += player1_roll
         if player1_roll == 1:
             print(f'{player1_name} rolled a {player1_roll}! Critical Fail')
         elif player1_roll == dice_size:
             print(f'{player1_name} rolled a {player1_roll}! Critical Success!')
         else:
             print(f'{player1_name} rolled a {player1_roll}')
-    if dice_rolls > 1:    
-        print(f'{player1_name} has rolled a total of {dice_sum}')
-def rolling2():
-    dice_sum = 0
+dice_sum_p2 = 0
+def player2_roll():
     for i in range(0, dice_rolls):
+        global player2_roll
         player2_roll = random.randint(1, dice_size)
-        dice_sum += player2_roll
+        global dice_sum_p2 
+        dice_sum_p2 += player2_roll
         if player2_roll == 1:
             print(f'{player2_name} rolled a {player2_roll}! Critical Fail')
         elif player2_roll == dice_size:
             print(f'{player2_name} rolled a {player2_roll}! Critical Success!')
         else:
             print(f'{player2_name} rolled a {player2_roll}')
-    if dice_rolls > 1:    
-        print(f'{player2_name} has rolled a total of {dice_sum}')
-rolling1()
+player1_roll()
 if player2_var == True:
-    rolling2()
+    player2_roll()
+#to define results for player1 and player2
+if dice_rolls > 1:
+    player1_result = dice_sum_p1
+    player2_result = dice_sum_p2
+else:
+    player1_result = player1_roll
+    player2_result = player2_roll
+#to define the comparison function
+def compare_results():
+    if player1_result > player2_result:
+        print (f"The WINNER is {player1_name} with the score of {dice_sum_p1}, congrats! \U0001F973")
+    elif player2_result > player1_result:
+        print (f"The WINNER is {player2_name} with the score of {dice_sum_p2}, congrats! \U0001F973")
+    else:
+        print (f"It's a TIE! Ouch!")
+#to compare the scores 
+if player2_var == True:
+    compare_results()
+elif player2_var == False and dice_rolls > 1:
+    print(f'{player1_name} has rolled a total of {dice_sum_p1}')
