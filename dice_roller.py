5b8e5a28016d9fd7cb806faf5704d3643b1538c0
IlonaLouckova <Ilona.Louckova@gmail.com>
2021-05-19 02:35:11 +0200

    Fix bug

--- dice_roller.py
+++ dice_roller.py
@@ -18,7 +18,6 @@ dice_size = int(input('How many sides are the dice? '))
 dice_sum_p1 = 0
 def player1_rolls():
     for i in range(0, dice_rolls):
-        global player1_roll
         player1_roll = random.randint(1, dice_size)
         global dice_sum_p1
         dice_sum_p1 += player1_roll
@@ -31,7 +30,6 @@ def player1_rolls():
 dice_sum_p2 = 0
 def player2_rolls():
     for i in range(0, dice_rolls):
-        global player2_roll
         player2_roll = random.randint(1, dice_size)
         global dice_sum_p2 
         dice_sum_p2 += player2_roll
@@ -45,26 +43,33 @@ player1_rolls()
 if player2_var == True:
     player2_rolls()
 #to define results for player1 and player2
-if dice_rolls > 1:
-    player1_result = dice_sum_p1
-    player2_result = dice_sum_p2
-else:
-    player1_result = player1_roll
-    player2_result = player2_roll
-#to define comparison function and rematch option
+global player1_result
+global player2_result
+player1_result = dice_sum_p1
+player2_result = dice_sum_p2
+#to define comparison function
 def compare_results():
+    global player1_result
+    global player2_result
     if player1_result > player2_result:
-        print (f'The WINNER is {player1_name} with the score of {dice_sum_p1}, congrats! \U0001F973')
+        print (f'The WINNER is {player1_name} with the score of {player1_result}, congrats! \U0001F973')
     elif player2_result > player1_result:
-        print (f'The WINNER is {player2_name} with the score of {dice_sum_p2}, congrats! \U0001F973')
+        print (f'The WINNER is {player2_name} with the score of {player2_result}, congrats! \U0001F973')
     else:
-        print (f"Well, that's a bummer, it's a TIE with {dice_sum_p1} each. \U0001F631")
+        print (f"Well, that's a bummer, it's a TIE with {player1_result} each. \U0001F631")
     global rematch
+    #rematch option loop
     rematch = yesorno("Do you want a rematch")
     if rematch == True:
+        global dice_sum_p1
+        dice_sum_p1 = 0
+        global dice_sum_p2
+        dice_sum_p2 = 0
         player1_rolls()
         player2_rolls()
-        compare_results() #THIS IS ALWAYS ADDING UP SCORES INSTEAD OF RESETING - fix TBD
+        player1_result = dice_sum_p1
+        player2_result = dice_sum_p2
+        compare_results()
 #final scores and comparison
 if player2_var == True:
     compare_results()
